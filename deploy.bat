31717bb642401b84b6402da1c8c2cf302e8a692b
Alex <afshtein@gmail.com>
2026-04-15 20:04:09 -0700

    - Added FireBase deployment automation + GitHub integration (deploys automatically on push, supposedly)

---
+++ deploy.bat
@@ -0,0 +1,11 @@
+@echo off
+del /q public\* 2>nul
+copy index.html public\
+copy main.js public\
+copy state.js public\
+copy physics.js public\
+copy render.js public\
+copy input.js public\
+copy controls.js public\
+copy style.css public\
+firebase deploy
