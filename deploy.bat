10c8fbaa28518213a814ff601332c22a871ac297
Fainstain, Eugene <Eugene.Fainstain@amd.com>
2026-04-30 18:09:29 -0700

    Revert "Renamed Zen1 folder to zen1 folder..."
    This reverts commit ca9ee29c540cfbdb9d10f45991dfcbd4b5076c68.

--- deploy.bat
+++ deploy.bat
@@ -23,10 +23,8 @@ copy input.js public\
 copy sound.js public\
 copy controls.js public\
 copy style.css public\
-rmdir /s /q public\Zen1 2>nul
-rmdir /s /q public\zen1 2>nul
-mkdir public\zen1
-for %%f in (zen1\*) do copy /y %%f public\zen1\
+if not exist public\Zen1 mkdir public\Zen1
+for %%f in (zen1\*) do copy /y %%f public\Zen1\
 if "%1"=="" (
   firebase deploy
 ) else (
