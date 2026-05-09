943be5b21a88f8d57c46f7cb695666bb1e13a178
Alex <afshtein@gmail.com>
2026-05-09 00:43:42 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,14 @@
 set -euo pipefail
 
 rm -rf public
-mkdir -p public/Alex public/Eugene public/Zen1
+mkdir -p public/Alex public/Alex1 public/Eugene public/Zen1
 
 cp index.html main.js state.js playfield.js og-preview.png \
    controller-spring-drag.js render.js input.js sound.js controls.js style.css \
    public/
 
 cp Alex/* public/Alex/
+cp Alex1/* public/Alex1/
 cp Eugene/* public/Eugene/
 cp Zen1/* public/Zen1/
 
